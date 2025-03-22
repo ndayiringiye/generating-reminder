5bb14c5b78f9862846720a761f8f9ce8e55dd20e
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-03-22 18:11:04 +0200

    feat:/ find sum has been successfully

--- reminder.js
+++ reminder.js
@@ -1,1 +1,12 @@
 console.log("hello word");
+
+function myFunction(a, b) {
+       return a + b;
+    }
+     
+    console.log(myFunction(1, 2))
+    // expected: 3 
+    console.log(myFunction(1, 10))
+    // expected: 11 
+    console.log(myFunction(99, 1))
+    // expected: 100 

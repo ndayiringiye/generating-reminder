068c1c5761ac53abda8d7ffeb5c37f93228c473f
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-03-22 18:49:18 +0200

    feat:/ toggling text in dom munipulation successfully

--- reminder.js
+++ reminder.js
@@ -1,20 +1,28 @@
 console.log("hello word");
 
 function myFunction(a, b) {
-       return a + b;
-    }
-     
-    console.log(myFunction(1, 2))
-    console.log(myFunction(1, 10))
-    console.log(myFunction(99, 1))
+    return a + b;
+}
 
-    function myStrickComparison(a, b) {
-           return a === b ;
-        }
-         
-        console.log(myStrickComparison(2, 3))
-        // expected: false 
-        console.log(myStrickComparison(1, '1'))
-        // expected: false 
-        console.log(myStrickComparison('10', '10'))
-        // expected: true 
+console.log(myFunction(1, 2))
+console.log(myFunction(1, 10))
+console.log(myFunction(99, 1))
+
+function myStrickComparison(a, b) {
+    return a === b;
+}
+
+console.log(myStrickComparison(2, 3))
+console.log(myStrickComparison(1, '1'))
+console.log(myStrickComparison('10', '10'))
+
+const buttonElem = document.getElementById("button");
+buttonElem.addEventListener('click', () => {
+    const oldText = buttonElem.innerText;
+    return button.innerText = oldText === "ON" ? "OFF" : "ON";
+});
+
+buttonElem.addEventListener('click', () => {
+    const oldText = buttonElem.innerText;
+    return button.innerText = oldText === "ON" ? "OFF" : "ON";
+});
